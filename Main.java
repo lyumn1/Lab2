7fe9ec30d4353ac670b87cec549f6af56cea252f
Mengfan Lyu <lyumn@mail.uc.edu>
2021-01-26 07:56:42 +0000

    Lab 2

--- Main.java
+++ Main.java
@@ -1,5 +1,28 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("M&M Color Counts");
+
+    System.out.println("Total = " + 11*55);
+
+    double input = 11*55* .24; 
+    System.out.println("Blue ：" + String.format("%.2f", input));
+
+     double i2 = 11*55* .14; 
+    System.out.println("Brown ：" + String.format("%.2f", i2));
+
+    double i3 = 11*55* .16; 
+    System.out.println("Green ：" + String.format("%.2f", i3));
+
+    double i4 = 11*55* .20; 
+    System.out.println("Orange ：" + String.format("%.2f", i4));
+
+    double i5 = 11*55* .13; 
+    System.out.println("Red ：" + String.format("%.2f", i5));
+
+    double i6 = 11*55* .14; 
+    System.out.println("Yellow ：" + String.format("%.2f", i6));
+
+    int n1, n2, n3, n4, n5, n6;
+    n1 = 11*55* .24
   }
 }
